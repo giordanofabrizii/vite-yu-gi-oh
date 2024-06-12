8fc4fd0e3509ae4bb83c78a61b93a9fb492edb3a
giordanofabrizii <giordanofabrizi@gmail.com>
2024-06-12 14:15:27 +0200

    Added Header section

--- src/components/HeaderApp.vue
+++ src/components/HeaderApp.vue
@@ -9,9 +9,20 @@ export default{
 </script>
 
 <template>
-
+    <header class="limited container-fluid">
+        <div class="row">
+            <div class="col d-flex">
+                <img src="https://1000logos.net/wp-content/uploads/2024/02/Yu-Gi-Oh-Logo-2001.png" alt="">
+                <h1>Yu-Gi-Oh Api</h1>
+            </div>
+        </div>
+    </header>
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
+@use '../../node_modules/bootstrap/scss/bootstrap.scss' as *;
 
+    img{
+        width: 4rem;
+    }
 </style>
